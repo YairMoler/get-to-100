873fef9badc26994c5cd6ad4e954d34081bb774d
naamaWyler <naamawyler@hilma.tech>
2024-11-03 16:30:48 +0200

    maybe working login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,15 +1,15 @@
 import { useState } from "react"
 
-const allUsers = []
-if (localStorage.getItem("allUsers")===null){
-    localStorage.setItem("allUsers", JSON.stringify(allUsers))
-}
-
-
 
 export default function Login({setPlayingUsers}){
-
+    
     const [username, setUserName] = useState("");
+    
+    
+    const allUsers = []
+    if (localStorage.getItem("allUsers")===null){
+        localStorage.setItem("allUsers", JSON.stringify(allUsers))
+    }
     
     function addToAllUsers (username){
         let users = JSON.parse(localStorage.getItem("allUsers"))
@@ -22,19 +22,25 @@ export default function Login({setPlayingUsers}){
     }
 
     function processUser(){
-        console.log("asdfsa")
+        let allUsers = JSON.parse(localStorage.getItem("allUsers"))
         let userIsNew = true
-        for (let i = 1; i< allUsers.length; i++){
+        for (let i = 0; i< allUsers.length; i++){
+            console.log("i'm in loop")
             if (username === allUsers[i].name){
-                
+                console.log("dave exists")
                 setPlayingUsers((prevPlayingUsers)=> [...prevPlayingUsers, allUsers[i]])
                 userIsNew = false;
+               // console.log("playingUsers: ",playingUsers)
                 break
             }
         }
         if (userIsNew){
             addToAllUsers (username);
-            setPlayingUsers((prevPlayingUsers)=>{[...prevPlayingUsers, allUsers[-1]]})
+            setPlayingUsers((prevPlayingUsers)=>{
+                return [...prevPlayingUsers, allUsers[allUsers.length-1]]
+               // console.log("playingUsers : ",playingUsers)
+            }
+        )
         }
     }
 
