a9aa73250874185c848ebc395a4842ddb7d50384
YairMoller <yairmoller@hilma.tech>
2024-11-03 16:14:39 +0200

    start new game

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,14 @@ import "./App.css";
 import GameScreen from "./components/GameScreen";
 
 function App() {
-    const [playingUsers, setPlayingUsers] = useState([]);
+    const [playingUsers, setPlayingUsers] = useState([
+        { name: "a", score: [3, 4] },
+        { name: "b", score: [4, 5] },
+    ]);
+
+    const updateScore = (index) => {
+        setPlayingUsers();
+    };
     return (
         <>
             <GameScreen playingUsers={playingUsers} />
